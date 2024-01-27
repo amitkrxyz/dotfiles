ae0638b6f3d0107d47c65d4e6c8c3d202260210f
Amit Kumar <83487500+amitkrxyz@users.noreply.github.com>
2024-01-27 18:25:52 +0530

    Add yt-search

---
+++ scripts/yt-search.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# For instance url: https://api.invidious.io/
+URL="https://invidious.einfachzocken.eu"
+
+# replaceing spaces with %20
+query=$(echo "$*" | sed 's/ /%20/g')
+
+chose=$(curl -s "$URL/api/v1/search?type=video&q=$query" | jq .[] | jq "[.title, .author, .videoId]" | jq -r 'join("\t")' | fzf)
+
+title_author=$(echo "$chose" | cut -f 1,2)
+
+vid_url=$(echo "$chose" | cut -f3)
+
+printf "%s\thttps://youtu.be/%s\n" "$title_author" "$vid_url"
