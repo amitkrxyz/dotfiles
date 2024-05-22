c54113f56f0d12b4e31f3f0e87530b4e13ab6478
Amit Kumar <83487500+amitkrxyz@users.noreply.github.com>
2024-05-22 21:15:27 +0530

    some updates:<

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,88 +1,105 @@
 return {
-  {
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup({
-        app = 'browser'
-      })
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
-  },
-  -- tadmccorkle/markdown.nvim
-  {
-    "tadmccorkle/markdown.nvim",
-    ft = "markdown", -- or 'event = "VeryLazy"'
-    opts = {
-      -- configuration here or empty for defaults
-    },
-  },
-  -- codeium ai
-  {
-    'Exafunction/codeium.vim',
-    event = 'BufEnter'
-  },
-  -- The undo history visualizer for
-  { 'mbbill/undotree' },
-  {
-    -- Library of 35+ independent Lua modules improving overall Neovim
-    "echasnovski/mini.nvim",
-    version = false,
-    config = function()
-      require("mini.surround").setup()
-      require("mini.comment").setup()
-      -- require("mini.ai").setup()
-    end,
-  },
-  {
-    -- Indent guides for Neovim
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {},
-    config = function()
-      require("ibl").setup()
-    end,
-  },
-  {
-    -- Statusline plugin written in pure lua.
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({})
-    end,
-  },
-  {
-    -- autopairs for neovim written by lua
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-  },
-  {
-    -- A lua plugin to manage multiple terminal windows
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    config = function()
-      require("toggleterm").setup {
-        open_mapping = [[<c-\>]],
-        direction = 'float'
-      }
-    end
-  },
-  {
-    'brenoprata10/nvim-highlight-colors',
-    config = function()
-      require('nvim-highlight-colors').setup {}
-    end
-  },
-  {
-    'akinsho/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'stevearc/dressing.nvim', -- optional for vim.ui.select
-    },
-    config = true,
-  }
+	"tpope/vim-sleuth",
+	-- folke/trouble.nvim
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	-- Markdown viewer
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup({
+				syntax = true,
+				app = "browser",
+			})
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
+	-- tadmccorkle/markdown.nvim
+	{
+		"tadmccorkle/markdown.nvim",
+		ft = "markdown", -- or 'event = "VeryLazy"'
+		opts = {
+			-- configuration here or empty for defaults
+		},
+	},
+	-- codeium ai
+	{
+		"Exafunction/codeium.vim",
+		event = "BufEnter",
+	},
+	-- The undo history visualizer for
+	{ "mbbill/undotree" },
+	{
+		-- Library of 35+ independent Lua modules improving overall Neovim
+		"echasnovski/mini.nvim",
+		version = false,
+		config = function()
+			require("mini.surround").setup()
+			require("mini.comment").setup()
+			-- require("mini.ai").setup()
+		end,
+	},
+	-- {
+	-- 	-- Indent guides for Neovim
+	-- 	"lukas-reineke/indent-blankline.nvim",
+	-- 	main = "ibl",
+	-- 	opts = {
+	-- 		scope = {
+	-- 			show_start = false,
+	-- 		},
+	-- 	},
+	-- 	-- config = function()
+	-- 	-- 	require("ibl").setup()
+	-- 	-- end,
+	-- },
+	{
+		-- Statusline plugin written in pure lua.
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({})
+		end,
+	},
+	{
+		-- autopairs for neovim written by lua
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equalent to setup({}) function
+	},
+	{
+		-- A lua plugin to manage multiple terminal windows
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				open_mapping = [[<c-\>]],
+				direction = "float",
+			})
+		end,
+	},
+	{
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
+	},
+	{
+		"akinsho/flutter-tools.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim", -- optional for vim.ui.select
+		},
+		config = true,
+	},
 }
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,103 +1,117 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "rafamadriz/friendly-snippets",
-    "saadparwaiz1/cmp_luasnip",
-    "j-hui/fidget.nvim",
-    'windwp/nvim-autopairs',
-    "nvimtools/none-ls.nvim",
-  },
-  config = function()
-    local ensure_installed = { "lua_ls" }
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+		"windwp/nvim-autopairs",
+		"nvimtools/none-ls.nvim",
+		"onsails/lspkind.nvim",
+	},
+	config = function()
+		local ensure_installed = { "lua_ls" }
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-    local lspconfig = require("lspconfig")
-    local handlers = {
-      -- The first entry (without a key) will be the default handler
-      -- and will be called for each installed server that doesn't have
-      -- a dedicated handler.
-      function(server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup { capabilities = capabilities }
-      end,
+		local lspconfig = require("lspconfig")
+		local handlers = {
+			-- The first entry (without a key) will be the default handler
+			-- and will be called for each installed server that doesn't have
+			-- a dedicated handler.
+			function(server_name) -- default handler (optional)
+				require("lspconfig")[server_name].setup({ capabilities = capabilities })
+			end,
 
-      ["lua_ls"] = function()
-        lspconfig.lua_ls.setup { settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" }
-            }
-          }
-        }
-        }
-      end,
-      -- ["tsserver"] = function()
-      --   lspconfig.tsserver.setup {
-      --     root_dir = lspconfig.util.root_pattern("package.json")
-      --   }
-      -- end,
-      -- ["denols"] = function()
-      --   lspconfig.denols.setup {
-      --     root_dir = lspconfig.util.root_pattern("deno.json")
-      --   }
-      -- end,
-    }
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = ensure_installed,
-      handlers = handlers
-    })
-    require("fidget").setup()
-    -- local luasnip = require("luasnip")
-    local cmp = require("cmp")
-    require("luasnip").filetype_extend("javascript", { "jsdoc" })
-    require("luasnip.loaders.from_vscode").lazy_load()
-    cmp.setup({
-      experimental = {
-        ghost_text = true
-      },
-      snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			["lua_ls"] = function()
+				lspconfig.lua_ls.setup({
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+						},
+					},
+				})
+			end,
+			["tsserver"] = function()
+				lspconfig.tsserver.setup({
+					root_dir = lspconfig.util.root_pattern("package.json"),
+					single_file_support = false,
+				})
+			end,
+			["denols"] = function()
+				lspconfig.denols.setup({
+					root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+				})
+			end,
+		}
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = ensure_installed,
+			handlers = handlers,
+		})
+		require("fidget").setup()
+		-- local luasnip = require("luasnip")
+		local cmp = require("cmp")
+		require("luasnip").filetype_extend("javascript", { "jsdoc" })
+		require("luasnip.loaders.from_vscode").lazy_load()
+		cmp.setup({
+			experimental = {
+				ghost_text = true,
+			},
+			snippet = {
+				-- REQUIRED - you must specify a snippet engine
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			window = {
+				-- completion = cmp.config.window.bordered(),
+				-- documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lua" },
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+			}, {
+				{ name = "path" },
+				{ name = "buffer" },
+			}),
+			formatting = {
+				format = require("lspkind").cmp_format({
+					-- mode = "symbol",
+					maxwidth = function()
+						return math.floor(0.25 * vim.o.columns)
+					end,
+					ellipsis_char = "...",
+					menu = {
+						buffer = "[buf]",
+						nvim_lsp = "[LSP]",
+						nvim_lua = "[api]",
+						path = "[path]",
+						luasnip = "[snip]",
+					},
+				}),
+			},
+		})
 
-      }),
-      sources = cmp.config.sources({
-        { name = 'nvim_lua' },
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' }, -- For luasnip users.
-      }, {
-        { name = 'path' },
-        { name = 'buffer' },
-      })
-    })
-
-
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    cmp.event:on(
-      'confirm_done',
-      cmp_autopairs.on_confirm_done()
-    )
-  end
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
