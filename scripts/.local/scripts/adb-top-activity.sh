6a260d31137a454d1c12fdf7c72a34efa8c5625f
Amit Kumar <83487500+amitkrxyz@users.noreply.github.com>
2025-01-18 08:46:36 +0530

    not using adbi alias

--- scripts/.local/scripts/adb-top-activity.sh
+++ scripts/.local/scripts/adb-top-activity.sh
@@ -3,4 +3,4 @@
 # Get the top Activity from adb
 # alias adbi='adb -s $(adb devices | grep \'device$\' | cut -f1 | fzf -1)'
 
-adbi shell dumpsys activity top | grep "ACTIVITY" | awk '{ print $2 }' | cut -d/ -f1 | tac
+adb shell dumpsys activity top | grep "ACTIVITY" | awk '{ print $2 }' | cut -d/ -f1 | tac
